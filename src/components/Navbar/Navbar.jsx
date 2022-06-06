18c8e14abc83d6d293ef48de2c4de95880c4ea7a
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-06-06 16:59:05 +0530

    commit 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
+import {Link} from 'react-router-dom';
 // use react-router Link or NavLink
-const Link = <a />;
+// const Link = <a />;
 
 const Navbar = () => {
+  const handleClick = () =>{}
   return (
     <div data-cy="navbar">
-      <Link data-cy="navbar-home-link"></Link>
-      <span data-cy="navbar-cart-items-count">{/* count here */}</span>
-      <button data-cy="navbar-login-logout-button"></button>
+      <Link  data-cy="navbar-home-link" to='/'>Logo</Link>
+      <span data-cy="navbar-cart-items-count">Cart:{0
+        /* count here */
+      
+      }</span>
+      <button data-cy="navbar-login-logout-button" onClick={handleClick}>Logout</button>
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,29 @@ import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    email: "eve.holt@reqres.in",
+    password: "cityslicka",
+  });
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  
+  };
+  const onSubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
     <div>
-      <input data-cy="login-email" />
-      <input data-cy="login-password" />
-      <button data-cy="login-submit"></button>
+    <form onSubmit={{onSubmit}}>
+     Login
+      <input data-cy="login-email" onChange={handleOnChange}  name="email" type="email" placeholder="Enter Email" />
+      <input data-cy="login-password" onChange={handleOnChange}  name="password" type="password" placeholder="Enter Password" />
+      <button data-cy="login-submit">Login</button>
+      </form>
     </div>
   );
 };
